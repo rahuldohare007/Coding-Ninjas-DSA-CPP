61d00617ffa08eeeaa617cfbf8f7d1939d949cf5
Rahul Dohare <67474480+rahuldohare007@users.noreply.github.com>
2022-08-14 12:23:55 +0530

    Create A4. Pair sum in array.cpp

---
+++ 4. Time and Space Complexity Analysis/A4. Pair sum in array.cpp
@@ -0,0 +1,42 @@
+/*
+You have been given an integer array/list(ARR) and a number 'num'. Find and return the total number of pairs in the array/list which sum to 'num'.
+Note:
+Given array/list can contain duplicate elements. 
+
+Sample Input 1:
+1
+9
+1 3 6 2 5 4 3 2 4
+7
+Sample Output 1:
+7
+Sample Input 2:
+2
+9
+1 3 6 2 5 4 3 2 4
+12
+6
+2 8 10 5 -2 5
+10
+Sample Output 2:
+0
+2
+
+Explanation for Input 2:
+Since there doesn't exist any pair with sum equal to 12 for the first query, we print 0.
+
+For the second query, we have 2 pairs in total that sum up to 10. They are, (2, 8) and (5, 5).
+*/
+
+int pairSum(int *arr, int n, int num)
+{
+     int count=0;
+     for(int i=0;i<n;i++){
+         for(int j=i+1;j<n;j++){
+             if(arr[i]+arr[j]==num){
+                 count++;
+             }
+         }
+     }
+ return count;
+}
