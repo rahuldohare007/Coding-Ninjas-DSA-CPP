67724c4bf646a2ee4094477c09e1a7f6fbbd5b90
Rahul Dohare <67474480+rahuldohare007@users.noreply.github.com>
2022-08-14 12:16:49 +0530

    Create A1. Find the Unique Element.cpp

---
+++ 4. Time and Space Complexity Analysis/A1. Find the Unique Element.cpp
@@ -0,0 +1,31 @@
+/*
+You have been given an integer array/list(ARR) of size N. Where N is equal to [2M + 1].
+Now, in the given array/list, 'M' numbers are present twice and one number is present only once.
+You need to find and return that number which is unique in the array/list.
+ Note:
+Unique element is always present in the array/list according to the given condition.
+
+Sample Input 1:
+1
+7
+2 3 1 6 3 6 2
+Sample Output 1:
+1
+Sample Input 2:
+2
+5
+2 4 7 2 7
+9
+1 3 1 3 6 6 7 10 7
+Sample Output 2:
+4
+10
+*/
+
+int findUnique(int *arr, int n) {
+    int res = arr[0];
+    for (int i = 1; i < n; i++)
+        res = res ^ arr[i];
+ 
+    return res;
+}
